e3aee3143b41f1c00361f53d103d453d73212c67
phuchoang29 <changchayabcd@gmail.com>
2023-06-15 00:16:26 +0700

    new

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,27 +1,30 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import LandingPage from './components/pages/LandingPage'
 import LoginPage from './components/pages/LoginPage'
 import RegisterPage from './components/pages/RegisterPage'
 import ForgetPasswordPage from './components/pages/ForgetPasswordPage'
 import HomePage from './components/pages/HomePage'
+import Hotel from "./components/pages/hotel/Hotel";
+import List from "./components/pages/list/List";
 
 import './App.css'
+
 
 export default function App() {
     return (
         <Router>
-            <div>
-                <Switch>
-                    <Route exact path="/" component={ LandingPage } />
-                    <Route path="/login" component={ LoginPage } />
-                    <Route path="/register" component={ RegisterPage } />
-                    <Route path="/forget-password" component={ ForgetPasswordPage } />
-                    <Route path="/home" component={ HomePage } />
-                </Switch>
-                <Footer />
-            </div>
+            <Switch>
+                <Route exact path="/" component={ LandingPage }/>
+                <Route path="/login" component={ LoginPage }/>
+                <Route path="/register" component={ RegisterPage }/>
+                <Route path="/forget-password" component={ ForgetPasswordPage }/>
+                <Route path="/home" component={ HomePage }/>
+                <Route path="/hotels" component={List}/>
+                <Route path="/hotel" component={Hotel}/>
+            </Switch>
+            <Footer />
         </Router>
     )
 }
--- webapp/src/components/pages/HomePage.js
+++ webapp/src/components/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet';
-import { Link} from 'react-router-dom'
+import {BrowserRouter, Link} from 'react-router-dom'
 import '../../HomePage.css'
 import SignInPage from './LoginPage';
 
@@ -12,12 +12,12 @@ export default function HomePage() {
                 <title>Home page</title>
 
             </Helmet>
-            
+
             <div className="left-class">
                 <a href="javascript:void(0)" className="link">Home page</a>
                 <a href="#" className="link">Room map</a>
                 <a href="#" className="link">Fellow guest</a>
-                <a href="#" className="link">Group guest</a>  
+                <a href="#" className="link">Group guest</a>
                 <a href="#" className="link">Reception</a>
                 <a href="#" className="link">Rooms</a>
                 <a href="#" className="link">Statistical</a>
@@ -26,8 +26,8 @@ export default function HomePage() {
             <div className="center-class">
                 <h1 className="main-title home-page-title">welcome</h1>
                 {/* <Link to="/">
-                    <button className="primary-button">Log out</button>
-                </Link> */}
+                <button className="primary-button">Log out</button>
+            </Link> */}
             </div>
         </div>
     )
