33f6d7a1b6d94f0063f0dcb738c93ffc6a3d7deb
Vi Dang Khoa <ahkhoats@gmail.com>
2023-06-24 14:58:24 +0700

    featured

--- web-ui/src/pages/home/Home.jsx
+++ web-ui/src/pages/home/Home.jsx
@@ -1,14 +1,24 @@
-import Navbar from "../../components/navbar/NavBar"
-import Header from "../../components/header/Header"
-import "./home.css"
+import Featured from "../../components/featured/Featured";
+// import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+// import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+// import MailList from "../../components/mailList/MailList";
+import Navbar from "../../components/navbar/NavBar";
+import PropertyList from "../../components/propertyList/PropertyList";
+import "./home.css";
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
-      <Header/>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
