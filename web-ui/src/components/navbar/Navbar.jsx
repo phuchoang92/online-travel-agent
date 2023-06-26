4bee2d9ad5eb76392413ad17307529fb76baf34a
phuchoang29 <changchayabcd@gmail.com>
2023-06-26 14:43:58 +0700

    Update Navbar.jsx

--- web-ui/src/components/navbar/Navbar.jsx
+++ web-ui/src/components/navbar/Navbar.jsx
@@ -4,11 +4,10 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Booking</span>
+        <span className="logo">lamabooking</span>
         <div className="navItems">
-            <button className="navButton">List your Home</button>
-            <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
         </div>
       </div>
     </div>
